249b4371ecce242bc62210cd9ef1f42a185c966a
Adela Sobotkova <adela@fedarch.org>
2018-07-04 08:44:54 +1000

    initial commit

---
+++ LeafletBackgrounds.R
@@ -0,0 +1,42 @@
+###   FINDING BACKGROUNDS TO WORK WITH
+
+
+## Choose favorite backgrounds in:
+# https://leaflet-extras.github.io/leaflet-providers/preview/
+## beware that some need extra options specified
+
+# Packages
+install.packages("leaflet")
+
+# Go
+library(leaflet)
+
+popup = c("Robin", "Jakub", "Jannes")
+
+leaflet() %>%
+  addProviderTiles("Esri.WorldPhysical") %>% 
+ #addProviderTiles("Esri.WorldImagery") %>% 
+  addAwesomeMarkers(lng = c(-3, 23, 11),
+                    lat = c(52, 53, 49), 
+                    popup = popup)
+
+
+## Navigate to Sydney
+leaflet() %>%
+  addTiles() %>%
+  addProviderTiles("Esri.WorldImagery", 
+                   options = providerTileOptions(opacity=0.5)) %>% 
+  setView(lng = 151.005006, lat = -33.9767231, zoom = 10)
+
+
+# Background: Europe
+leaflet() %>% 
+  addTiles() %>% 
+  setView( lng = 2.34, lat = 48.85, zoom = 5 ) %>% 
+  addProviderTiles("Esri.WorldPhysical", group = "Physical") %>% 
+  addProviderTiles("Esri.WorldImagery", group = "Aerial") %>% 
+  addProviderTiles("MtbMap", group = "Geo") %>% 
+
+addLayersControl(
+  baseGroups = c("Geo","Aerial", "Physical"),
+  options = layersControlOptions(collapsed = T))
